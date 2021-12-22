0970160fad0ce88d775b431a60689cf59447009e
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-12-22 17:28:09 -0500

    created Modal for use within PhotoList

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
-    setContactSelected
+    currentCategory,
+    setContactSelected,
   } = props;
 
   return (
@@ -24,19 +24,18 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => setContactSelected(true)}>
-              Contact
-            </span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && `navActive`
+                currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
-              <span onClick={() => {
+              <span
+                onClick={() => {
                   setCurrentCategory(category);
                   setContactSelected(false);
                 }}
